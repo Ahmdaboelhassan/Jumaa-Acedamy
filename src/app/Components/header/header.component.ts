333404e414abade82b4da82f9102de5d83bf583d
ahmdaboelhassan <ahmedaboelhassan1000@gmail.com>
2025-11-07 02:51:44 +0200

    Intial

---
+++ src/app/Components/header/header.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent implements OnInit {
+  logoPath = signal('assets/logo-light.png');
+  isDarkMode = false;
+  menuOpen = signal(false);
+
+  ngOnInit() {
+    const storedTheme = localStorage.getItem('theme');
+
+    if (storedTheme) {
+      this.isDarkMode = storedTheme === 'dark';
+    } else {
+      this.isDarkMode = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      ).matches;
+      localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+    }
+
+    this.updateTheme();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+    this.updateTheme();
+  }
+
+  private updateTheme() {
+    const root = document.documentElement;
+    if (this.isDarkMode) {
+      root.classList.add('dark');
+      this.logoPath.set('assets/logo-dark.png');
+    } else {
+      root.classList.remove('dark');
+      this.logoPath.set('assets/logo-light.png');
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpen.update((open) => !open);
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => HomeComponent, pathMatch: 'full' },
+  { path: 'Home', loadComponent: () => HomeComponent },
+  { path: '**', loadComponent: () => HomeComponent },
+];
