dbcfb80cd62330b54fdc6ef9ed137e1c72f3c160
ahmdaboelhassan <ahmedaboelhassan1000@gmail.com>
2025-11-15 02:13:44 +0200

    Finish Our Courses Section

--- Client/src/app/Components/our-courses/our-courses.component.ts
+++ Client/src/app/Components/our-courses/our-courses.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { MainLandingComponent } from '../main-landing/main-landing.component';
+import { PillarsOfLearningComponent } from './pillars-of-learning/pillars-of-learning.component';
+import { OurProgramsComponent } from '../home/our-programs/our-programs.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 @Component({
   selector: 'app-our-courses',
-  imports: [MainLandingComponent],
+  imports: [
+    MainLandingComponent,
+    PillarsOfLearningComponent,
+    OurProgramsComponent,
+    HowItWorksComponent,
+  ],
   templateUrl: './our-courses.component.html',
   styleUrl: './our-courses.component.css',
 })
