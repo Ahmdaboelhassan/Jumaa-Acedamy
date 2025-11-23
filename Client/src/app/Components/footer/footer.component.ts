4308640bfcefbb75f7de401f1a8ced8f26b6c12e
ahmdaboelhassan <ahmedaboelhassan1000@gmail.com>
2025-11-23 23:38:17 +0200

    Repair Responsive

--- Client/src/app/Components/footer/footer.component.ts
+++ Client/src/app/Components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed } from '@angular/core';
 import { DarkModeService } from '../../Services/dark-mode.service';
 import { FooterIconsComponent } from './footer-icons/footer-icons.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [FooterIconsComponent],
+  imports: [FooterIconsComponent, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
